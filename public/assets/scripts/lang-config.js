08e632aca04195ac875c8d06cc757ac2b426d2f8
tulyaTim <tulyatimo@gmail.com>
2024-09-28 14:28:12 +0300

    Add files via upload

---
+++ public/assets/scripts/lang-config.js
@@ -0,0 +1,8 @@
+window.__GOOGLE_TRANSLATION_CONFIG__ = {
+    languages: [
+      { title: "English", name: "en" },
+      { title: "Luganda", name: "lg" }, 
+      { title: "Swahili", name: "sw" }, 
+    ],
+    defaultLanguage: "en",
+  };
